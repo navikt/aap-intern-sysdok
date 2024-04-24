d8ee99824d29d4a52e93c0ed58b8ca0cce7271d1
Øivind Stensrud <oivind.stensrud@nav.no>
2024-04-24 11:44:26 +0200

    Retter feil som førte til at mermaid-diagrammer ikke ble vist
    Fjernet plugins som ikke var nødvendige og endret config fra
    cjs til ems

--- dokumentasjon/docusaurus.config.js
+++ dokumentasjon/docusaurus.config.js
@@ -1,93 +1,90 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer").themes.github;
-const darkCodeTheme = require("prism-react-renderer").themes.dracula;
+import { themes as prismThemes } from "prism-react-renderer";
 
-async function createConfig() {
-  const mdxMermaid = await import("mdx-mermaid");
+/** @type {import('@docusaurus/types').Config} */
+export default {
+  title: "Litt om AAP",
+  tagline: "",
+  url: "https://aap-sysdoc.intern.nav.no",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/nav_logo.png",
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
 
-  return {
-    title: "Litt om AAP",
-    tagline: "",
-    url: "https://aap-sysdoc.intern.nav.no",
-    baseUrl: "/",
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
-    favicon: "img/nav_logo.png",
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "nb",
+    locales: ["nb"],
+  },
 
-    // Even if you don't use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
-    i18n: {
-      defaultLocale: "nb",
-      locales: ["nb"],
-    },
-
-    presets: [
-      [
-        "classic",
-        {
-          docs: {
-            sidebarPath: require.resolve("./sidebars.js"),
-            remarkPlugins: [mdxMermaid.default],
-            // Please change this to your repo.
-            // Remove this to remove the "edit this page" links.
-            editUrl:
-              "https://github.com/navikt/aap-intern-sysdok/edit/main/dokumentasjon",
-          },
-
-          theme: {
-            customCss: require.resolve("./src/css/custom.css"),
-          },
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/navikt/aap-intern-sysdok/edit/main/dokumentasjon",
         },
-      ],
+
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
     ],
+  ],
 
-    themeConfig:
-      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-      ({
-        navbar: {
-          title: "Dokumentasjon",
-          logo: {
-            alt: "AAP Logo",
-            src: "img/aap_logo.png",
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Dokumentasjon",
+        logo: {
+          alt: "AAP Logo",
+          src: "img/aap_logo.png",
+        },
+        items: [
+          {
+            href: "https://github.com/navikt/aap-intern-sysdok",
+            label: "GitHub",
+            position: "right",
           },
-          items: [
-            {
-              href: "https://github.com/navikt/aap-intern-sysdok",
-              label: "GitHub",
-              position: "right",
-            },
-          ],
-        },
-        footer: {
-          style: "dark",
-          links: [
-            {
-              title: "Vi pleier å være her",
-              items: [
-                {
-                  label: "Slack",
-                  href: "https://nav-it.slack.com/archives/C031DUS37DK",
-                },
-                {
-                  label: "Zoom",
-                  href: "https://nav-it.zoom.us/j/92869347604?pwd=NjZCNXpQblB4b2pkNUtoRVArSTZCUT09",
-                },
-              ],
-            },
-          ],
-        },
-        prism: {
-          theme: lightCodeTheme,
-          darkTheme: darkCodeTheme,
-        },
-        mermaid: {
-            theme: { light: "dark", dark: "dark" }
-        },
-      }),
-  };
-}
-
-module.exports = createConfig;
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Vi pleier å være her",
+            items: [
+              {
+                label: "Slack",
+                href: "https://nav-it.slack.com/archives/C031DUS37DK",
+              },
+              {
+                label: "Zoom",
+                href: "https://nav-it.zoom.us/j/92869347604?pwd=NjZCNXpQblB4b2pkNUtoRVArSTZCUT09",
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+      mermaid: {
+        theme: { light: "dark", dark: "dark" },
+      },
+    }),
+};
